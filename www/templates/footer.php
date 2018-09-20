3b94b7d9e1dececef38264dff1dbb5d7d4718cdf
Kaleb Jubar <ktjubar@vt.edu>
2018-09-20 11:30:24 -0400

    Updated templates for better practice

--- www/templates/footer.php
+++ www/templates/footer.php
@@ -1,8 +1,22 @@
 
-<footer>
-    <p>Created By:</p>
-    <p>Kipp Dunn (<a href="mailto:kipp34@vt.edu">kipp34@vt.edu)</a> and Harison Gertler (<a href="mailto:hsg32@vt.edu">hsg32@vt.edu</a>).</p>
-    <strong>Scottie and Kippernicus Inc.</strong>
-</footer>
+    <footer>
+        <p>Created By:</p>
+        <p>Kipp Dunn (<a href="mailto:kipp34@vt.edu">kipp34@vt.edu)</a> and Harison Gertler (<a href="mailto:hsg32@vt.edu">hsg32@vt.edu</a>).</p>
+        <strong>Scottie and Kippernicus Inc.</strong>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="application/javascript"></script>
+    <script type="application/javascript">
+        $(function() {
+            $('#myForm').on('submit', function(e) {
+                e.preventDefault();
+                var p = $("<p>").text($("#input").val());
+                $("#data").append(p);
+                $("#input").val("");
+            });
+        });
+    </script>
+
+</body>
 
 </html>
--- www/templates/header.php
+++ www/templates/header.php
@@ -5,24 +5,15 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="main.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>
-        $(function() {
-            $('#myForm').on('submit', function(e) {
-                e.preventDefault();
-                var p = $("<p>").text($("#input").val());
-                $("#data").append(p);
-                $("#input").val("");
-            });
-        });
-    </script>
 </head>
 
-<header>
-    <div class="topnav">
-        <a href="index">Home</a>
-        <a href="challenges">Challenges</a>
-        <a href="about">About</a>
-        <a href="contact">Contact</a>
-    </div>
-</header>
+<body>
+
+    <header>
+        <div class="topnav">
+            <a href="index">Home</a>
+            <a href="challenges">Challenges</a>
+            <a href="about">About</a>
+            <a href="contact">Contact</a>
+        </div>
+    </header>
