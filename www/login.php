62c9e72a83dd3a01875ce528749136133a417bf8
kippdunn <kipp34@vt.edu>
2018-09-24 15:21:39 -0400

    Fix credentials placeholders

--- www/login.php
+++ www/login.php
@@ -1,13 +1,13 @@
 <?php $title = 'Login to play!'; include("templates/header.php"); ?>
 
 <body>
-    
+
     <h1>Login to ESC:wq!</h1>
 
     <center>
-        <form id="myForm">
-            <div><input id="input" placeholder="Add your text"></div>
-            <div><input id="input" placeholder="Add your text"></div>
+        <form id="credentials">
+            <div><input id="username" placeholder="User Name"></div>
+            <div><input id="password" placeholder="Password"></div>
         </form>
     <center>
 <body>
